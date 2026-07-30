70b74571c367ee21deeac258fc7d7d67ae01099d
nina-bilirossi <84231436+nina-bilirossi@users.noreply.github.com>
2026-07-30 09:01:03 +0000

    Deploying to gh-pages from @ nina-bilirossi/climate-labor-story@5ad4f56ac706b34dbb83789cf0d1e67f1d1c2c1a 🚀

---
+++ assets/SiteHeader-B_BGbQJz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B2qO5JNF.js";import{T as e}from"./TopNav-CVkbtN-F.js";function t(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{visible:!0}),r.jsx("div",{"aria-hidden":!0,className:"h-[52px]"})]})}export{t as S};
